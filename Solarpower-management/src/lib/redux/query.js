c54c3c2f104cfaca991eb7e0d3a5030ee1bf764f
MircoFernando <mircofdo@gmail.com>
2025-10-26 20:29:22 +0530

    customised the sidebar and edited the chart

--- Solarpower-management/src/lib/redux/query.js
+++ Solarpower-management/src/lib/redux/query.js
@@ -1,16 +1,34 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
+import dotenv from "dotenv";
+dotenv.config();
 
 const BASE_URL = "http://localhost:3000/api";
+const WEATHER_API = process.env.WEATHER_API; // e.g. "https://api.openweathermap.org/data/2.5"
 
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     getenergyGenerationRecord: builder.query({
-        query: ({id, groupBy }) => `energy-generation-records/solar-units/${id}?groupBy=${groupBy}`,
+      query: ({ id, groupBy, range }) =>
+        `energy-generation-records/solar-units/${id}?groupBy=${groupBy}${range ? `&range=${range}` : ""}`,
     }),
-    }),
+  }),
 });
 
 export const { useGetenergyGenerationRecordQuery } = api;
+
+// Weather API client
+export const weatherApi = createApi({
+  reducerPath: "weatherApi",
+  baseQuery: fetchBaseQuery({ baseUrl: WEATHER_API }),
+  endpoints: (builder) => ({
+    // Only current weather
+    getWeather: builder.query({
+      query: ({ lat, lon, appid }) =>
+        `weather?lat=${lat}&lon=${lon}&appid=${appid}`,
+    }),
+  }),
+});
+
+export const { useGetWeatherQuery } = weatherApi;
